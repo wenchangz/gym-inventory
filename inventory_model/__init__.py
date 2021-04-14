bb84b4b0d5004123d3c4c801aaebfe8c01fd382e
wenchangz <82208659+wenchangz@users.noreply.github.com>
2021-04-14 21:53:13 +0800

    Update __init__.py

--- inventory_model/__init__.py
+++ inventory_model/__init__.py
@@ -4,3 +4,8 @@ register(
     id='inventory_model-v0',
     entry_point='inventory_model.envs:Inventory',
 )
+
+register(
+    id='inventory_cont_model-v0',
+    entry_point='inventory_model.envs:ContInventory',
+)
