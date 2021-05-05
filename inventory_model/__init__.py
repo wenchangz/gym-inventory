8289bc9858b92dbc8e2d42f5fe075ab753b93403
ZhiLiu724 <zl724@cornell.edu>
2021-05-05 14:45:42 +0800

    updated a new environment with a few fixes

--- inventory_model/__init__.py
+++ inventory_model/__init__.py
@@ -14,3 +14,13 @@ register(
     id='inventory_cont_config_model-v0',
     entry_point='inventory_model.envs:ContInventoryConfig',
 )
+
+register(
+    id='inventory_cont_config_validation_model-v0',
+    entry_point='inventory_model.envs:ContInventoryConfigValidation',
+)
+
+register(
+    id='inventory_cont_config_fix_model-v0',
+    entry_point='inventory_model.envs:ContInventoryConfigFix',
+)
---
+++ inventory_model/envs/inventory_cont_env_config_fix.py
@@ -0,0 +1,73 @@
+import gym
+import numpy as np
+import sys
+
+class ContInventoryConfigFix(gym.Env):
+    metadata = {'render.modes': ['human']}
+    
+    def __init__(self, **config): 
+    # def __init__(self, **config): 
+        
+#         self.h = config['h'] # the holding cost
+#         self.p = config['p'] # the penalty cost
+#         self.L = config['L'] # leading time
+#         self.lamb = config['lambda'] # demand rate
+        #self.starting_state = config['starting_state'] # set initial state
+        
+        self.h = config['h'] # the holding cost
+        self.p = config['p'] # the penalty cost
+        self.L = config['L'] # leading time
+        self.lamb = config['lambda'] # demand rate
+        
+        self.action_space = gym.spaces.Box(low=0.0, high=5.0, shape=(1,),dtype=np.float32) # definition of the action space
+
+        # self.action_space = gym.spaces.Discrete(20)
+
+        
+        self.observation_space = gym.spaces.Box(low=0.0, high=np.inf, shape=(self.L+1,1), dtype=np.float32) # definition of the state space
+        
+        #self.state = np.asarray(self.starting_state) 
+        self.state = np.array([0.0]*(self.L+1)) 
+
+        self.seed()
+
+
+    def seed(self, seed=None):
+        self.np_random, seed = gym.utils.seeding.np_random(seed)
+        return [seed]
+
+   
+    def reset(self):
+        #self.state = np.asarray(self.starting_state)
+        self.state = np.array([0.0]*(self.L+1)) 
+        res = self.state
+        res.shape = (self.L+1,1)
+        return res
+    
+    
+    def step(self, action):
+        assert self.action_space.contains(action)
+        
+        demand = np.random.exponential(self.lamb,1)
+        
+        # update inventory
+        loss = -min(0, self.state[0] + self.state[1] - demand)
+        self.state[0] = max(0, self.state[0] + self.state[1] - demand)
+        #total inventory no more than N: this may lead to some problems with holding cost?
+        
+
+        # update pipeline vector x
+        for i in range(1,self.L):
+            self.state[i] = self.state[i+1]
+        self.state[self.L] = action
+        
+        # compute the reward
+#         loss = -min(0, self.state[0] + self.state[1] - demand)
+        reward = -self.h*self.state[0] - self.p*loss
+
+        return self.state, reward, False, {}
+    
+#     def render(self , mode=’human’):
+#         outfile = sys.stdout if mode == ’human’ else super(Inventory , self).render(mode=mode)
+#         outfile.write(np.array2string(self.state)+'\n')
+       
